90ef3c8dc89bd46b41231ecd6bd735b8e67e0845
kanandev2024 <kananramazandev@gmail.com>
2024-02-21 17:54:54 +0100

    [3-55]: Creating a Recipe Model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes = [];
 }
