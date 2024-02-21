29d67554344ef922f487e50b469c8b753831cc3b
kanandev2024 <kananramazandev@gmail.com>
2024-02-21 12:52:56 +0100

    [3-54]: Alternative Non-Collapsable Navigation Bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  collapsed = true;
+}
