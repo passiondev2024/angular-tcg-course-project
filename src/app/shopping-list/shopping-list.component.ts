21d85d1cbb0568d79c8dc26ccec94d62d244a569
kanandev2024 <kananramazandev@gmail.com>
2024-02-23 17:05:12 +0100

    [3-59]: Working on the ShoppingListComponent

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-
+  ingredients = [];
 }
