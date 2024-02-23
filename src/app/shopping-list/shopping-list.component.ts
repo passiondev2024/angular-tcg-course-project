2363bb5705d0de555f7392c1e6b8f42e9ab99f82
kanandev2024 <kananramazandev@gmail.com>
2024-02-23 17:10:39 +0100

    [3-61]: Creating and Outputting the Shopping List

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 }
