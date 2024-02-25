784ce369ad58ee5d66f20b045f59a2393e7bc007
kanandev2024 <kananramazandev@gmail.com>
2024-02-25 20:47:27 +0100

    [NA] Rename of repo and project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularTcgTheBasics
+# AngularTcgCourseProject
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tcg-the-basics'`, () => {
+  it(`should have as title 'angular-tcg-course-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-tcg-the-basics');
+    expect(app.title).toEqual('angular-tcg-course-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-tcg-the-basics');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angular-tcg-course-project'
+    );
   });
 });
