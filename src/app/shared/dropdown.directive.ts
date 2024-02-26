19cff52302a36897e8c0dee0da12d800a358f184
kanandev2024 <kananramazandev@gmail.com>
2024-02-26 18:08:01 +0100

    [8-106]: Closing the Dropdown From Anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -23,7 +23,10 @@ export class DropdownDirective {
 
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
+  constructor(private elRef: ElementRef) {}
 }
