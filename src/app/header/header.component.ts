94e949d138cb4d9bdb82b2d39f2eac0f21a3f890
kanandev2024 <kananramazandev@gmail.com>
2024-03-15 13:33:27 +0100

    [19-285]: Storing Recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  BASE_URL =
+    'https://angular-tcg-course-project-default-rtdb.europe-west1.firebasedatabase.app';
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    console.log(`🔎 | DataStorageService | storeRecipes`);
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(`${this.BASE_URL}/recipes.json`, recipes)
+      .subscribe((response) => {
+        console.log(
+          `🔎 | DataStorageService | storeRecipes > response:`,
+          response
+        );
+      });
+  }
 }
